8d8b31fc7c692845f15d109e6d77ea65f0c5e242
JamesTheN00B <hiranmaydas1999@gmail.com>
2021-03-10 23:42:08 +0530

    Comments added as best as I could

--- TicTacToe.py
+++ TicTacToe.py
@@ -104,18 +104,18 @@ class TicTacToe:
             self.printBoard()
             position = input(f"Enter Move for {playerID} (1-9): ")
             os.system("clear") if os.name == "posix" else os.system("cls")
-            try:
+            try:    # Check if user entered integer or not
                 position = int(position)
-            except ValueError:
+            except ValueError:  # If entered character is not integer
                 print("\nPlease enter an Integer.\n")
                 continue
-            if position < 1 or position > 9:
+            if position < 1 or position > 9:    # If entered integer is out of range
                 print("\nInteger must be within range 1-9.\n")
                 continue
             else:
-                try:
+                try:    # Inserting at position
                     self.insertAtPosition(position, playerID)
-                except InvalidMove:
+                except InvalidMove:  # In case the position is already occupied
                     print("\nThat square is already occupied. Please enter valid input.")
                     continue
                 break
@@ -125,8 +125,8 @@ class TicTacToe:
     # Return Type - None
     # Function - Inserts symbol of the second player in a random empty spot
     def CPU_easy(self):
-        options_left = [key for key, value in self.board.items() if value.isdigit()]
-        self.insertAtPosition(random.choice(options_left), self.player2)
+        options_left = [key for key, value in self.board.items() if value.isdigit()]    # Empty positions available
+        self.insertAtPosition(random.choice(options_left), self.player2)    # Randomly choosing an empty position
 
     # Function Name - CPU_intermediate
     # Parameters - None
@@ -135,19 +135,19 @@ class TicTacToe:
     #            Otherwise, checks if there exists a spot where player 1 can win. If yes, then it blocks that move.
     #            If neither are available, it places its symbol in a random grid.
     def CPU_intermediate(self):
-        options_left = [key for key, value in self.board.items() if value.isdigit()]
-        for option in options_left:
+        options_left = [key for key, value in self.board.items() if value.isdigit()]    # Empty positions available
+        for option in options_left:  # Checking if there is any position where the CPU can place to win
             self.board[option] = self.player2
             if self.playerWon(self.player2):
                 return
             self.board[option] = str(option)
-        for option in options_left:
+        for option in options_left:  # Checking if there is any position where the player can place to win and block it
             self.board[option] = self.player1
             if self.playerWon(self.player1):
                 self.board[option] = self.player2
                 return
             self.board[option] = str(option)
-        self.insertAtPosition(random.choice(options_left), self.player2)
+        self.insertAtPosition(random.choice(options_left), self.player2)    # Randomly choosing an empty position
 
     # Function Name - miniMax
     # Parameters - depth (Integer, stores the depth of the current recursion tree)
@@ -158,34 +158,34 @@ class TicTacToe:
     #            possible if minimising, else returns the highest score possible if maximising, as all the moves
     #            made must be assumed to be optimal.
     def miniMax(self, depth, isMaximising, alpha, beta):
-        if self.playerWon(self.player1):
+        if self.playerWon(self.player1):    # If the Minimizer won
             return -10 + depth
-        if self.playerWon(self.player2):
+        if self.playerWon(self.player2):    # If the Maximizer won
             return 10 - depth
-        if self.isBoardFull():
+        if self.isBoardFull():  # If its a Tie
             return 0
-        if isMaximising:
+        if isMaximising:    # If it's the Maximizer's turn
             best_score = -math.inf
             for i in range(1, 10):
-                if self.isPositionEmpty(i):
+                if self.isPositionEmpty(i):  # Trying out all the possible moves in a state space tree
                     self.board[i] = self.player2
                     score = self.miniMax(depth + 1, False, alpha, beta)
                     best_score = max(best_score, score)
                     alpha = max(alpha, best_score)
                     self.board[i] = str(i)
-                    if beta <= alpha:
+                    if beta <= alpha:   # If the maximum score is already found
                         break
             return best_score
-        else:
+        else:   # If it's the Minimizer's turn
             best_score = math.inf
             for i in range(1, 10):
-                if self.isPositionEmpty(i):
+                if self.isPositionEmpty(i):  # Trying out all the possible moves in a state space tree
                     self.board[i] = self.player1
                     score = self.miniMax(depth + 1, True, alpha, beta)
                     best_score = min(best_score, score)
                     beta = min(beta, best_score)
                     self.board[i] = str(i)
-                    if beta <= alpha:
+                    if beta <= alpha:   # If the minimum score is already found
                         break
             return best_score
 
@@ -198,14 +198,14 @@ class TicTacToe:
         best_move = -1
         high_score = -math.inf
         for i in range(1, 10):
-            if self.isPositionEmpty(i):
+            if self.isPositionEmpty(i):  # Finding the best move to make
                 self.board[i] = self.player2
                 score = self.miniMax(0, False, -math.inf, math.inf)
                 self.board[i] = str(i)
                 if score > high_score:
                     best_move = i
                     high_score = score
-        self.board[best_move] = self.player2
+        self.board[best_move] = self.player2    # Making the best move
         return
 
     # Function Name - gameTime
@@ -222,7 +222,7 @@ class TicTacToe:
         choice = input("Enter Choice : ")
         AI = {'1': 'player', '2': 'easy', '3': 'intermediate', '4': 'hardcore'}
         os.system("clear") if os.name == "posix" else os.system("cls")
-        self.startGame(AI[choice])
+        self.startGame(AI[choice])  # Starting the game as per user's preference
 
     # Function Name - startGame
     # Parameters - level (String, stores the type of opponent the player faces)
@@ -232,18 +232,18 @@ class TicTacToe:
     #            no more moves to play. Displays the result accordingly.
     def startGame(self, level):
         isCPU = (level != 'player')
-        while True:
-            if self.isBoardFull():
+        while True:  # Infinite Loop to continue the game till a winner is found or game is tied
+            if self.isBoardFull():  # Draw Check
                 self.gameDraw()
                 break
-            self.playerMove(self.player1)
-            if self.playerWon(self.player1):
+            self.playerMove(self.player1)   # The player's (X) move
+            if self.playerWon(self.player1):    # X's win check
                 self.gameWon(self.player1, False)
                 break
-            if self.isBoardFull():
+            if self.isBoardFull():  # Draw Check
                 self.gameDraw()
                 break
-            if level == 'player':
+            if level == 'player':   # Preparing opponent as per player's preference
                 self.playerMove(self.player2)
             elif level == 'easy':
                 self.CPU_easy()
@@ -251,7 +251,7 @@ class TicTacToe:
                 self.CPU_intermediate()
             elif level == 'hardcore':
                 self.CPU_hardcore()
-            if self.playerWon(self.player2):
+            if self.playerWon(self.player2):    # O's win check
                 self.gameWon(self.player2, isCPU)
                 break
 
